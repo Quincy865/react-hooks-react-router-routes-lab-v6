d6cb65224e621748763b153f5e4d8fd304ae2e98
Quincy Michael <michaelkirima865@gmail.com>
2024-11-18 19:53:37 +0300

    JSX lab

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
-import "./index.css";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import routes from './routes';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <RouterProvider router={createBrowserRouter(routes)} />
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,28 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import MovieCard from '../components/MovieCard';
 
-function Home() {
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
-    </>
+    <div>
+      <h1>Home Page</h1>
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
   );
 };
 
 export default Home;
+
+
+
